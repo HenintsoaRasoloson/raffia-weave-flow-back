b224af3228d962a5f1dfa9c5b8a3ee1fb68c2dfa
Rasoloson Henintsoa <har@esokia-webagency.com>
2026-07-08 16:51:25 +0300

    feat: update AppModule to include Clients, Deliveries, Invoices, Production Orders, and Sales Orders modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule } from './clients/clients.module';
+import { DeliveriesModule } from './deliveries/deliveries.module';
+import { InvoicesModule } from './invoices/invoices.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ProductionOrdersModule } from './production-orders/production-orders.module';
+import { ProductsModule } from './products/products.module';
+import { SalesOrdersModule } from './sales-orders/sales-orders.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    ClientsModule,
+    ProductsModule,
+    SalesOrdersModule,
+    ProductionOrdersModule,
+    DeliveriesModule,
+    InvoicesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
